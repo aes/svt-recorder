1d45f7b3ff71f0f17e71c9793faa42a62567d49b
Anders Eurenius <aes@spotify.com>
2014-01-09 23:16:21 +0100

    Massive refactoring, subtitle downloading, more

--- lib/svt/recorder.rb
+++ lib/svt/recorder.rb
@@ -1,41 +1,263 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
+require 'cgi'
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+require 'pp'
+require 'progress_bar'
 
-#--
-# Since I'm planning to use this library both in Ruby 1.9.2 and 1.8.7
-# and 1.9.2 removes . from LOAD_PATH add this directory to the load path
-$:.push File.expand_path(File.dirname(__FILE__))
-
-require 'recorder/play'
-require 'recorder/rapport'
-require 'recorder/oppetarkiv'
 
 module SVT #:nodoc:
-  # == Summary
-  # This library will give you the URL:s for recording files from
-  # SVT Play and Play rapport.
-  #
-  # Read the documentation for the Play class for how to use
-  # those classes directly.
   module Recorder
-    # To facilitate easy recordings you can use this method to easily
-    # instantiate the right class depending on the URL passed in.
-    #
-    # If the URL matches playrapport.se it will use the Rapport class,
-    # else the Play class.
-    #
-    # Returns an array consisting of:
-    # <tt>[base_url, [parts], bitrate, video_title]</tt>
-    def self.record(url)
-      if url.match(/svtplay\.se/)
-        SVT::Recorder::Play.record(url)
-      elsif url.match(/oppetarkiv\.se/)
-        SVT::Recorder::OppetArkiv.record(url)
-      else
-        SVT::Recorder::Rapport.record(url)
+    VERSION = '1.1.0'
+
+    class Base
+      def initialize(url, options)
+        @subs       = nil
+        @part_base  = ''
+        @parts      = []
+        @stream_map = {}
+        @options    = options
+
+        video = fetch_playlist(url)
+
+        @title      = video[:title] or ''
+        @stream     = video[:url]
+        @base_url   = File.dirname(video[:url])
+
+        self.part_urls
+      end
+
+      attr_reader :subs
+      attr_reader :parts
+      attr_reader :title
+      attr_reader :base_url
+      attr_reader :stream_map
+
+      def playlist_config(url)
+        {
+          /svtplay\.se/ =>
+          ['#player', "http://www.svtplay.se/%s?output=json"],
+
+          /oppetarkiv\.se/ =>
+          ['#player', "http://www.oppetarkiv.se/%s?output=json"],
+
+          /svt\.se/ =>
+          ['.svtplayer', "http://www.svt.se/%s"],
+        }.each do |pattern, config|
+          if url.match(pattern)
+            return config
+          end
+        end
+
+        raise "Unknown hostname"
+      end
+
+      def fetch_playlist_url(url)
+        css_path, playlist_format = playlist_config(url)
+
+        doc = Nokogiri::HTML(open(url).read)
+        player = doc.at_css(css_path)
+        stream = CGI.unescape(player['data-json-href'])
+        return playlist_format % stream
+      end
+
+      def fetch_playlist(url)
+        stream = fetch_playlist_url(url)
+        jsinfo = open(stream).read
+        open('/tmp/info.json', 'w') {|f| f.write(jsinfo) }
+
+        @jsinfo = JSON.parse(jsinfo)
+
+        if not @jsinfo['video']['availableOnMobile']
+          raise ArgumentError, "The passed in URL is not available for mobile"
+        end
+
+        title = @jsinfo['context']['title']
+        v = @jsinfo['video']['videoReferences'].find({}) do |v|
+          v['playerType'] == 'ios'
+        end
+
+        @subs = @jsinfo['video']['subtitleReferences'][0]['url'] rescue nil
+        if @subs == ''
+          @subs = nil
+        end
+
+        if not v.empty?
+            @title = title
+            @url = CGI.unescape(v['url'])
+            {:title => title, :url => @url} if not v.empty?
+        end
+      end
+
+      def choose_stream
+        if @options[:resolution]
+          url, _ = get_streams().find do |k,v|
+            v['RESOLUTION'] == @options[:resolution]
+          end
+        elsif @options[:bitrate]
+          url, _ = get_streams().find do |k,v|
+            v['BANDWIDTH'].to_i == @options[:bitrate].to_i
+          end
+        else
+          bitrate = bitrates.max
+
+          url, _ = get_streams().find do |k,v|
+            v['BANDWIDTH'].to_i == bitrate
+          end
+        end
+
+        url
+      end
+
+      def part_urls
+        return if not @parts.empty?
+
+        url = choose_stream
+
+        open(url).each do |row|
+          next if row[0..0] == '#'
+          row.strip!
+
+          @part_base = File.dirname(row) if @part_base.empty?
+          part = File.basename(row)
+          @parts << "#{@part_base}/#{part}"
+        end
+      end
+
+      # Returns or yields all parts, in order, for this video.
+      # If all parts then are downloaded in sequence and concatenated there
+      # will be a playable movie.
+      #
+      # Yield:
+      #   A complete part download URL
+      #
+      # Returns:
+      #   All parts in an ordered array, first -> last, full URL
+      #def parts
+      #  if block_given?
+      #    @parts.each {|i| yield "#{@part_base}/#{i}"}
+      #  else
+      #    @parts.map {|p| "#{@part_base}/#{p}" }
+      #  end
+      #end
+
+      # Returns the number of parts this recording got
+      #
+      # Returns:
+      #   int the numbers of parts, 0 index
+      def parts?
+        return @parts.size
+      end
+
+      # All available bitrates for this video/playlist.
+      # Returns:
+      #   An array of bitrates, orderered highest->lowest
+      def bitrates
+        bitrates = Hash[get_streams().map {|url,v| [v['BANDWIDTH'].to_i, url]}]
+        bitrates.keys.sort.reverse
+      end
+
+      #--
+      # A naïve parser, but until it turns out to be a problem it'll do.
+      # 2012=09-09: If a FQDN address is given only return the basename
+      #
+      # The format is:
+      #   EXT-X-.... BANDWIDTH=<bitrate>
+      #   playlist-filename
+      def get_streams
+        return @stream_map if not @stream_map.empty?
+
+        bitrate = nil
+
+        rates = open(@stream).read
+        open('/tmp/streaminfo.wtf', 'w') {|f| f.write(rates) }
+
+        @stream_map = {}
+
+        rates.scan(/\n#EXT-X-STREAM-INF:([^\n]*)\n(http:[^\n]*)/) do |kv,url|
+          @stream_map[url] = Hash[kv.scan(/([^=,]+)=("[^"]*"|[^,]*)/)]
+        end
+
+        @stream_map
+      end
+    end # /Base
+
+    class HTTPDownload
+      def initialize(options, downloader, filename)
+        @options = options
+        @base_url = URI.parse(downloader.base_url)
+        @downloader = downloader
+        @headers = {
+          'User-Agent' =>
+          ('Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.2.10) ' +
+           'Gecko/20100914 Firefox/3.6.10')
+        }
+        @filename = filename
+        @server = Net::HTTP.start(@base_url.host, @base_url.port)
+      end
+
+      def fetch_subs
+        if @downloader.subs
+          subs_name = @filename + '.srt'
+          STDERR.puts "Getting subs..."
+          File.open(subs_name, 'wb+') do |fh|
+            text = open(@downloader.subs).read
+            fixed = text.gsub(/(\d\d:\d\d:\d\d).(\d\d\d)/, '\1,\2')
+            fh.write(fixed)
+          end
+        end
+      end
+
+      # Will yield the number of megabytes downloaded every megabyte
+      def fetch_video
+        downloaded = 0
+        megabyte = 1024 * 1024
+        mb_down = 0
+
+        STDERR.puts "Starting recording..."
+
+        File.open(@filename + '.mp4', 'wb+') do |fh|
+          @downloader.parts.each_index do |i|
+            part = @downloader.parts[i]
+            begin
+              @server.request_get(part, @headers) do |res|
+                res.read_body do |body|
+                  fh.write body
+                end
+              end # /@server
+            rescue Timeout::Error, EOFError, Errno::ECONNRESET => exception
+              yield -1
+              @server = Net::HTTP.start(@base_url.host, @base_url.port)
+              STDERR.puts "Connection error..."
+              retry
+            end
+
+            yield i, part
+          end
+        end
+
+        STDERR.puts "\nFinished recording: #{@filename}"
+      end
+
+      def close ; @server.finish ; end
+      alias :disconnect :close
+
+      def get
+
+        fetch_subs if @options[:subs]
+
+        if @options[:quiet]
+          prog = lambda {|i, part| }
+        else
+          progress = ProgressBar.new(
+              @downloader.parts.size, :bar, :elapsed, :eta, :counter)
+          prog = lambda {|i, part| progress.increment! }
+        end
+
+        fetch_video &prog if @options[:video]
       end
     end
-
-    VERSION = '1.0.3'
   end
 end
